653b6e8d4e904c196c307569c01a7dc20aa3814a
fraric06 <fra.ric06@gmail.com>
2026-08-26 20:41:34 +0200

    Attiva V7: dati treno identici alla scheda ViaggiaTreno

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,6 @@
-const national=require('./national-loader-v6');
+const national=require('./national-loader-v7');
 (async()=>{
-  console.log('=== TrainRadar24 · Piemonte realtime verificato ===');
-  try{const x=await national.load();console.log(`Avvio realtime: stazioni=${x.status.stations} treni_verificati=${x.trips.length}`)}catch(e){console.error('Errore avvio realtime:',e.message)}
-  require('./server-national-v6');
+  console.log('=== TrainRadar24 · Piemonte realtime · dati verificati ViaggiaTreno ===');
+  try{const x=await national.load();console.log(`Avvio realtime V7: stazioni=${x.status.stations} treni_verificati=${x.trips.length}`)}catch(e){console.error('Errore avvio realtime:',e.message)}
+  require('./server-national-v7');
 })();
