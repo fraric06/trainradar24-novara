5a653a148a48fc4186e62708ed03727dfc77261d
fraric06 <fra.ric06@gmail.com>
2026-08-26 20:47:11 +0200

    Switch bootstrap to realtime V8

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,2 @@
-const national=require('./national-loader-v7');
-(async()=>{
-  console.log('=== TrainRadar24 · Piemonte realtime · dati verificati ViaggiaTreno ===');
-  try{const x=await national.load();console.log(`Avvio realtime V7: stazioni=${x.status.stations} treni_verificati=${x.trips.length}`)}catch(e){console.error('Errore avvio realtime:',e.message)}
-  require('./server-national-v7');
-})();
+const national=require('./national-loader-v8');
+(async()=>{console.log('=== TrainRadar24 · Piemonte realtime verificato V8 ===');try{const x=await national.load();console.log(`Avvio realtime: stazioni=${x.status.stations} treni_verificati=${x.trips.length}`)}catch(e){console.error('Errore avvio realtime:',e.message)}require('./server-national-v8')})();
