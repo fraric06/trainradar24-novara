663886e7a4e2ba6d3c1e8784f7c9e4ac75996093
fraric06 <fra.ric06@gmail.com>
2026-08-27 10:19:27 +0200

    Normalize routes delays and last detections before API responses

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,6 @@
 const national=require('./national-loader-v9');
-(async()=>{console.log('=== TrainRadar24 · Piemonte realtime verificato V10 ===');try{const x=await national.load();console.log(`Avvio realtime V10: stazioni=${x.status.stations} treni_verificati=${x.trips.length}`)}catch(e){console.error('Errore avvio realtime:',e.message)}require('./server-national-v9')})();
+const {normalizePayload}=require('./train-normalizer');
+const rawLoad=national.load.bind(national);
+national.load=async()=>normalizePayload(await rawLoad());
+national.realtime=national.load;
+(async()=>{console.log('=== TrainRadar24 · Piemonte realtime verificato V11 ===');try{const x=await national.load();console.log(`Avvio realtime V11: stazioni=${x.status.stations} treni_verificati=${x.trips.length}`)}catch(e){console.error('Errore avvio realtime:',e.message)}require('./server-national-v9')})();
