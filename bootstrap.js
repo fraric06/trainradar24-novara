b85a036fb65ba2f0dc2d8eb0024c789d83ce7275
fraric06 <fra.ric06@gmail.com>
2026-08-27 09:46:09 +0200

    chore: activate V9 realtime loader

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,2 @@
-const national=require('./national-loader-v8');
-(async()=>{console.log('=== TrainRadar24 · Piemonte realtime verificato V8 ===');try{const x=await national.load();console.log(`Avvio realtime: stazioni=${x.status.stations} treni_verificati=${x.trips.length}`)}catch(e){console.error('Errore avvio realtime:',e.message)}require('./server-national-v8')})();
+const national=require('./national-loader-v9');
+(async()=>{console.log('=== TrainRadar24 · Piemonte realtime verificato V9 ===');try{const x=await national.load();console.log(`Avvio realtime V9: stazioni=${x.status.stations} treni_verificati=${x.trips.length}`)}catch(e){console.error('Errore avvio realtime:',e.message)}require('./server-national-v9')})();
