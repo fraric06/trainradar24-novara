cd0bf962f3b32de021d8404acd947a3a61cab0a2
fraric06 <fra.ric06@gmail.com>
2026-08-27 10:06:49 +0200

    Update realtime bootstrap label to V10

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,2 @@
 const national=require('./national-loader-v9');
-(async()=>{console.log('=== TrainRadar24 · Piemonte realtime verificato V9 ===');try{const x=await national.load();console.log(`Avvio realtime V9: stazioni=${x.status.stations} treni_verificati=${x.trips.length}`)}catch(e){console.error('Errore avvio realtime:',e.message)}require('./server-national-v9')})();
+(async()=>{console.log('=== TrainRadar24 · Piemonte realtime verificato V10 ===');try{const x=await national.load();console.log(`Avvio realtime V10: stazioni=${x.status.stations} treni_verificati=${x.trips.length}`)}catch(e){console.error('Errore avvio realtime:',e.message)}require('./server-national-v9')})();
