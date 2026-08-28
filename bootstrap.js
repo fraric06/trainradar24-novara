07029332bfc9ab4b230d7dd36b880773dcec5fbf
fraric06 <fra.ric06@gmail.com>
2026-08-28 12:52:21 +0200

    Update startup banner for extended regional coverage

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,2 @@
-console.log('=== TrainRadar24 · Piemonte realtime verificato V13 ===');
+console.log('=== TrainRadar24 · Piemonte realtime verificato V14 + R25/R27/S6 estesi ===');
 require('./server-national-v9');
